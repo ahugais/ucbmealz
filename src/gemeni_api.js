5bf197314cd2a52f366e5dcc219b7e109e9dc1f0
Ibrashoukry <ibrahim.m.shoukry@gmail.com>
2024-10-19 04:26:32 -0700

    test

--- src/gemeni_api.js
+++ src/gemeni_api.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Write a story about a magic backpack.";
+const prompt = "Create a meal plan that has 50 grams of protein and 10 grams of carbs.";
 
 const result = await model.generateContent(prompt);
 console.log(result.response.text());
