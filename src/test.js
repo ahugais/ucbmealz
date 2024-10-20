6e17d2acc2c7a2ef5021f880a2e926eea5e38ed9
Ibrashoukry <ibrahim.m.shoukry@gmail.com>
2024-10-20 05:52:10 -0700

    sqlite3

--- src/test.js
+++
@@ -1,8 +0,0 @@
-const { mealData } = require("./mealsData");
-
-async function test() {
-    const mealDatas = await mealData();
-    const dataString = JSON.stringify(mealDatas);
-    return dataString;
-}
-console.log(test());
