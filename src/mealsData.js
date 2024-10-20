e5c0e6ea55cb42c7a7fad2469b33793217d8a2f9
Ibrashoukry <ibrahim.m.shoukry@gmail.com>
2024-10-20 07:04:57 -0700

    create json file

--- src/mealsData.js
+++ src/mealsData.js
@@ -1,22 +1,14 @@
 // sets up sql database
 const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
 
 // function that creates a Promise and returns all the values of the table
-function mealData() {
-  return new Promise((resolve, reject) => {
-
     const db = new sqlite3.Database('./dining_hall_data.db');
     db.all("SELECT * FROM scraped_data",
       (err, rows) => {
-        if(err) {
-          reject(err);
-        } else {
-          resolve(rows);
-        }
+        const jsonString = JSON.stringify(rows);
+        fs.writeFileSync('src/menuData.json', jsonString);
+        console.log(test);
         db.close();
       });
-  })
-}
-
-module.exports = { mealData };
   
